ba811d0a63379a141a1f098526afa127b4eb18fb
jgarrison87 <64297933+jgarrison87@users.noreply.github.com>
2022-04-21 09:57:28 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -9,3 +9,17 @@ phaseTwo.addEventListener('click', e => {
 function changeText() {
   machineCity.innerText('ACCESS DENIED');
 }
+
+
+// burger
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const navItems = document.querySelector('.nav-items');
+
+  burger.addEventListener('click', () => {
+    navItems.classList.toggle('nav-active');
+  });
+}
+
+navSlide();
